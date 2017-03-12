d7ca9a205062205c5cc69ea9106e3d54520135da
yolandayyn <yy693@cornell.edu>
2017-03-12 00:56:11 -0500

    Second Change

--- MyApplication5/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication5/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,24 @@
 package com.example.myapplication;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.media.MediaRecorder;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.widget.Toast;
 
 import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private ConversationService service;
@@ -31,10 +29,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private String text;
     private String responseWord;
-    private ToggleButton recBtn;
-    private MediaRecorder mr;
-    private File soundFile;
-    public static final int RADIO_REQUEST = 1;
+    private ImageButton recBtn;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private Map<String, Object> con;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
         btn = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.editText);
         textView = (TextView) findViewById(R.id.textView);
-        recBtn = (ToggleButton) findViewById(R.id.toggleButton2);
+        recBtn = (ImageButton) findViewById(R.id.imageButton2);
+        initializeService();
     }
 
     private class WastonTask extends AsyncTask<String, Void, String> {
@@ -54,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                 }
             });
-            initializeService();
             String a = process();
             return a;
         }
@@ -74,21 +71,38 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void recordingBtn(View view) {
-        if(recBtn.isChecked()){
-            startRec();
-        }
-        else{
-            stopRec();
+        promptSpeechInput();
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    editText.setText(result.get(0));
+                }
+                break;
+            }
+
         }
     }
 
         private String process(){
-            MessageRequest newMessage = new MessageRequest.Builder().inputText(text).build();
+            MessageRequest newMessage = new MessageRequest.Builder().inputText(text).context(con).build();
 
             String workspaceId = "8e90a03a-57f2-41fe-bdb8-87ad82aa770e";
 
             MessageResponse response = service.message(workspaceId, newMessage).execute();
             responseWord = response.getOutput().get("text").toString();
+            con = response.getContext();
             return responseWord;
         }
 
@@ -97,100 +111,22 @@ public class MainActivity extends AppCompatActivity {
             service.setUsernameAndPassword(wusername, wpassword);
         }
 
-        public void startRec() {
-            if(isExternalStorageWritable()) {
-                if (mr == null) {
-                    File dir = new File(Environment.getExternalStorageDirectory(), "sounds");
-                    if (!dir.exists()) {
-                        dir.mkdirs();
-                    }
-                    soundFile = new File(dir, System.currentTimeMillis() + ".amr");
-                    if (!soundFile.exists()) {
-                        try {
-                            soundFile.createNewFile();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    mr = new MediaRecorder();
-                    mr.reset();
-                    mr.setAudioSource(MediaRecorder.AudioSource.MIC);
-                    mr.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
-                    mr.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-                    mr.setOutputFile(soundFile.getAbsolutePath());
-                    try {
-                        mr.prepare();
-                        mr.start();  //开始录制
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        public void stopRec(){
-            if(mr != null){
-                mr.stop();
-                mr.release();
-                mr = null;
-            }
-        }
-
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-
-    public void checkRequest(){
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.RECORD_AUDIO)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.RECORD_AUDIO},
-                        RADIO_REQUEST);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
         }
     }
-    @Override
-    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case RADIO_REQUEST: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
     }
 
